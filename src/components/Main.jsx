0d36e2b21103916efdd04eee3c0475aeea22a3b3
NAOYUKI UMEDA <naoyuki.umd@hotmail.co.jp>
2023-08-23 23:18:35 +0900

    main箇所のhtmlを追加

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -4,9 +4,19 @@ import "./Main.css";
 
 const Main = () => {
     return (
-        <div>
-            <p>Main</p>
+        <>
+        <div className="app-main">
+            <div className="app-main-note-edit">
+                <input type="text"></input>
+                <textarea id="" placeholder="ノート内容を記入"></textarea>
+            </div>
         </div>
+
+        <div className="app-name-note-preview">
+            <h1 className="preview-title">タイトル</h1>
+            <div className="markdown-preview">ノート内容</div>
+        </div>
+        </>
     );
 };
 
