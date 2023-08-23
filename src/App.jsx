ada764897e0394e03b9aa70b88e9cd98b5e48739
NAOYUKI UMEDA <naoyuki.umd@hotmail.co.jp>
2023-08-23 21:46:24 +0900

    コンポーネント追加

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,14 @@ import Sidebar from "./components/Sidebar.jsx";
 import Main from "./components/Main.jsx";
 
 function App() {
+
+    const onAddNote = () => {
+        console.log("新しくノートが追加されました");
+    };
+
   return (
     <div className="App">
-        <Sidebar></Sidebar>
+        <Sidebar onAddNote={onAddNote}></Sidebar>
         <Main></Main>
     </div>
   )
